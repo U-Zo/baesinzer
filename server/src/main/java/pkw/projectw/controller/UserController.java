387cbe43665273d627079ffaf66810960ed60f34
u_zo <kwoojo0127@naver.com>
2020-10-26 19:58:07 +0900

    임시 로그인 기능 추가
    리액트 요청 테스트를 위한 테스트
    
    관련: #1

--- server/src/main/java/pkw/projectw/controller/UserController.java
+++ server/src/main/java/pkw/projectw/controller/UserController.java
@@ -1,14 +1,16 @@
 package pkw.projectw.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pkw.projectw.domain.User;
 import pkw.projectw.service.UserService;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/register")
-    public Map<String, String> signUp(@RequestBody RegisterForm form) {
+    public Map<String, String> register(@RequestBody RegisterForm form) {
         System.out.println(form.getEmail());
         User user = new User(form.getEmail(), form.getPassword());
         userService.register(user);
@@ -27,4 +29,17 @@ public class UserController {
 
         return userInfo;
     }
+
+    @PostMapping("/api/login")
+    public Map<String, String> login(@RequestBody Map<String, Long> map) {
+        Map<String, String> auth = new HashMap<>();
+        userService.findOne(map.get("id")).ifPresentOrElse(user -> {
+            auth.put("email", user.getEmail());
+        }, () -> {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "유저가 존재하지 않습니다.");
+        });
+
+        return auth;
+    }
+
 }
--- server/src/main/java/pkw/projectw/repository/UserRepository.java
+++ server/src/main/java/pkw/projectw/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package pkw.projectw.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pkw.projectw.domain.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
