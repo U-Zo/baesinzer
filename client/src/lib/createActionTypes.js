795adff1f757e4e0bd60ce20a9a240ef0200d155
Woojo Kim <kwoojo0127@naver.com>
2020-11-24 17:51:58 +0900

    사용하지 않는 모듈 삭제

--- client/src/lib/createActionTypes.js
+++
@@ -1,8 +0,0 @@
-const createActionTypes = (type) => {
-  const SUCCESS = `${type}_SUCCESS`;
-  const FAILURE = `${type}_FAILURE`;
-
-  return [type, SUCCESS, FAILURE];
-};
-
-export default createActionTypes;
