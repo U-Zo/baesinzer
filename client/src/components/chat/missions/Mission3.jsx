326ad24d87b7e65505b5d5693959da1c75d417af
Sejin Park <tpwls0504@gmail.com>
2020-12-09 17:53:42 +0900

    파일 백업하기 구현
    백업 start 클릭했을 때 게이지 차며
    게이지 꽉 찼을 때 Mission Clear! 출력
    Mission Done true로 바꾸기
    
    관련 #18#110

---
+++ client/src/components/chat/missions/Mission3.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { missionDone } from '../../../modules/user';
+import styled, { css, keyframes } from 'styled-components';
+import { ImFloppyDisk } from 'react-icons/im';
+// const SweepStyle = styled.input`
+//   padding: 0.75em;
+//   border: 1px var(--color-green) solid;
+//   background: linear-gradient(to right, var(--color-green), var(--color-green));
+//   background-repeat: no-repeat;
+//   background-size: 0 100%;
+//   transition: background-size 1s 0s;
+//   :hover {
+//     background-size: 100% 100%;
+//   }
+// `;
+
+const MissionBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 2rem;
+  justify-content: center;
+`;
+const MissionInfo = styled.div`
+  font-size: 4rem;
+  color: var(--color-green);
+`;
+const MissionClear = styled.div`
+  font-size: 4rem;
+  color: var(--color-red);
+  position: absolute;
+  text-align: center;
+  width: 45rem;
+`;
+const rotatePacman = keyframes`
+  from{
+    transform:rotate(0deg);
+    }
+  to{
+    transform:rotate(20deg);
+    }
+`;
+const ImFloppyDiskStyle = styled(ImFloppyDisk)`
+  font-size: 5rem;
+  margin-left: 1rem;
+  &#start {
+    display: inline-block;
+    animation: ${rotatePacman} 1s ease infinite;
+    /* animation: 1s move steps(1, start) infinite; */
+    animation-iteration-count: 6;
+  }
+`;
+const MissionGauge = styled.div`
+  width: 25rem;
+  height: 5rem;
+  border: 1px var(--color-green) solid;
+  background: linear-gradient(to right, var(--color-green), var(--color-green));
+  background-repeat: no-repeat;
+  background-size: 0 100%;
+  transition: background-size 5s 0s;
+  &#start {
+    background-size: 100% 100%;
+    text-align: center;
+  }
+`;
+const Mission3 = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const [start, setStart] = useState('');
+  const [complete, setComplete] = useState();
+
+  // const onClick = () => {
+  //   setStart('start');
+  //   setTimeout(() => {
+  //     setComplete('Mission Clear!');
+  //     dispatch(missionDone(3));
+  //   }, 6000);
+  // };
+
+  const missionRef = useRef(null);
+
+  setTimeout(() => {
+    setStart('start');
+  }, 0);
+
+  const timer = () =>
+    setTimeout(function () {
+      setComplete('파일 백업하기 완료');
+      dispatch(missionDone(3));
+    }, 6000);
+  useEffect(() => {
+    missionRef.current = timer();
+  }, []);
+
+  useEffect(() => {
+    if (complete) {
+      setTimeout(() => {
+        onClose();
+      }, 2000);
+    }
+  }, [complete]);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(missionRef.current);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* <SweepStyle type="submit" value="Send Request" class="sweep"></SweepStyle> */}
+      <MissionInfo>Please back up your data.</MissionInfo>
+      <MissionBlock>
+        <MissionGauge id={start} />
+        <ImFloppyDiskStyle id={start} />
+      </MissionBlock>
+      <MissionClear id={start}>{complete}</MissionClear>
+    </div>
+  );
+};
+
+export default Mission3;
