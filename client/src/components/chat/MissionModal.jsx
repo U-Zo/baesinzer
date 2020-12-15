bb3fab8fbb14a9640d1f96496b3b68bc09de2fdc
Woojo Kim <kwoojo0127@naver.com>
2020-12-15 18:18:45 +0900

    미션 12 사물함 열기 구현
    열쇠 아이콘에 ref를 맞춰 포커싱하여 keydown 이벤트 리스너 적용
    styled component의 props로 해당 위치의 좌표 값을 변경
    열쇠 아이콘과 자물쇠 아이콘이 겹쳐지면 docking을 true로 바꿔
    문 여는 동작으로 변경 로직은 위와 동일
    
    구현: #141

--- client/src/components/chat/MissionModal.jsx
+++ client/src/components/chat/MissionModal.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Modal from '../common/Modal';
 import Mission1 from './missions/Mission1';
 import Mission10 from './missions/Mission10';
+import Mission12 from './missions/Mission12';
 import Mission2 from './missions/Mission2';
 import Mission3 from './missions/Mission3';
 import Mission4 from './missions/Mission4';
@@ -51,6 +52,7 @@ const MissionModal = ({
         <Mission8 onClose={closeMissionModal} username={username} />
       )}
       {missionId === 10 && <Mission10 onClose={closeMissionModal} />}
+      {missionId === 12 && <Mission12 onClose={closeMissionModal} />}
       <MissionModalCloseButton onClick={closeMissionModal}>
         X
       </MissionModalCloseButton>
